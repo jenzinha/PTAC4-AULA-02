43b60518b33f3ef29da1b64b8847424027c0b3cb
jamili <jamili.amaral@estudante.ifms>
2023-08-02 00:06:25 -0400

    Update page.js

--- src/app/pages/private/page.js
+++ src/app/pages/private/page.js
@@ -4,10 +4,18 @@ import { useRouter } from 'next/navigation';
 
 export default function PagePrivate() {
    
+    const { push, refresh } = useRouter();
+    const handlerRemoveToken = () => {
+    Cookies.remove('token');
+    push('/pages/private');
+    refresh();
+  };
+
     return (
         <div>
             <h1>Página Privada</h1>
             <button onClick={() => handlerRemoveToken()}>Remover Token</button>
         </div>
     )
+    
 }
